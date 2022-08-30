9daf1e4d78f7236fd8eeda1627db0824e38dc6e7
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-08-31 00:24:11 +0500

    Editing Toto added, Code Improved

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function EmptyCheck(){
 function getAndupdate(){
     let tit = document.getElementById('title').value;
     let desc = document.getElementById('description').value;
-    if ((tit == "")){
+    if ((!tit.match(/([^\s])/))){
         document.getElementById("title-empty").style.display = "initial";
     }
     else{
@@ -54,9 +54,12 @@ function update(){
         str += `
             <tr>
               <th scope="row">${index+1}</th>
-              <td>${element[0]}</td>
-              <td>${element[1]}</td>
-              <td><buttton class="btn btn-sm btn-outline-danger" onclick="deleted(${index})">Delete</buttton></td>
+              <td class="todoTitle">${element[0]}</td>
+              <td class="todoDesc">${element[1]}</td>
+              <td>
+                <buttton class="btn btn-sm btn-outline-info mx-1" onclick="editTodo(${index}, event)">Edit</buttton>
+                <buttton class="btn btn-sm btn-outline-danger mx-1" onclick="deleted(${index})">Delete</buttton>
+              </td>
             </tr>`;
     });
     tablebody.innerHTML = str;
@@ -67,14 +70,38 @@ function update(){
 }
 
 function deleted(itemIndex){
-    itemsJsonArrayStr = localStorage.getItem('itemsJson');
-    itemsJsonArray = JSON.parse(itemsJsonArrayStr);
-    // Delete itemIndex from Array
     itemsJsonArray.splice(itemIndex, 1);
     localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray))
     update();
 }
+function editTodo(itemIndex, event){
+    var editRow = event.currentTarget.parentNode.parentNode
+    var allTD = editRow.getElementsByTagName('td')
+    editRow.innerHTML = `
+    <th scope="row">${itemIndex+1}</th>
+    <td><input type="text" value='${allTD[0].innerHTML}' class="form-control px-2 py-1 clear-title" id="changedTitle" placeholder="Title of your TO-DO"></td>
+    <td><input type="text" value='${allTD[1].innerHTML}' class="form-control px-2 py-1 clear-title" id="changedDesc" placeholder="Description of your TO-DO"></td>
+    <td>
+      <buttton class="btn btn-sm btn-outline-success py-1" onclick="changeTodo(${itemIndex})">Save Todo</buttton>
+    </td>
+    `
+}
 function clearStorage(){
-    localStorage.clear();
+    localStorage.removeItem('itemsJson')
+    update();
+}
+
+function changeTodo(itemIndex){
+    var title = document.getElementById('changedTitle').value
+    var desc = document.getElementById('changedDesc').value
+
+    if(!title.match(/([^\s])/)){
+        update()
+        return
+    }
+    var editedTodo = []
+    editedTodo.push(title, desc)
+    itemsJsonArray.splice(itemIndex, 1, editedTodo)
+    localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray))
     update();
 }
